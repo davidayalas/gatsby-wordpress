ef7e1485ca7bb4376be88b11ddea1a7b9aa15de3
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-20 13:12:17 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: 'wordpress.env' })
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -26,6 +28,30 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // your wordpress source
+        baseUrl: process.env.baseUrl,
+        protocol: process.env.protocol,
+        // is it hosted on wordpress.com, or self-hosted?
+        hostingWPCOM: process.env.hostingWPCOM,
+        // does your site use the Advanced Custom Fields Plugin?
+        useACF: process.env.useACF,
+        auth: {
+          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
+          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
+          // then add your clientId, clientSecret, username, and password here
+          // Learn about environment variables: https://www.gatsbyjs.org/docs/environment-variables
+          // If two-factor authentication is enabled then you need to create an Application-Specific Password,
+          // see https://en.support.wordpress.com/security/two-step-authentication/#application-specific-passwords
+          wpcom_app_clientSecret: process.env.wpcom_app_clientSecret,
+          wpcom_app_clientId: process.env.wpcom_app_clientId,
+          wpcom_user: process.env.wpcom_user,
+          wpcom_pass: process.env.wpcom_pass,
+        },        
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
